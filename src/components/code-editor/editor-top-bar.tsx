61d3648c7f24c5d14098ef3ad444e199ea77716b
ACBC <acbc.dev@gmail.com>
2024-11-19 17:51:35 -0500

    add toogle button for layout

--- src/components/code-editor/editor-top-bar.tsx
+++ src/components/code-editor/editor-top-bar.tsx
@@ -1,4 +1,4 @@
-import { Play, Plus, Minus, RotateCcw, Download, Copy, Trash2 } from 'lucide-react'
+import { Play, Plus, Minus, RotateCcw, Download, Copy, Trash2, Columns, Rows } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 import { Tooltip } from "@/components/ui/tooltip"
 import { useToast } from "@/hooks/use-toast"
@@ -7,7 +7,7 @@ import { EditorSettingsDialog } from './editor-setting-dialog'
 
 export function EditorTopBar() {
   const { toast } = useToast()
-  const { code, editorRef, monaco, getCurrentTheme, resetCode, clearConsole, runCode, setFontSize } = useEditorStore()
+  const { code, editorRef, monaco, getCurrentTheme, resetCode, clearConsole, runCode, setFontSize, setLayout, layout } = useEditorStore()
   const currentTheme = getCurrentTheme()
   const handleReset = () => {
     resetCode()
@@ -16,6 +16,9 @@ export function EditorTopBar() {
       description: "The editor has been reset to the default code.",
       duration: 2000
     })
+  }
+  const toogleLayout = () => {
+    setLayout(layout === "horizontal" ? "vertical" : "horizontal")
   }
   const handleClear = () => {
     clearConsole()
@@ -119,6 +122,19 @@ export function EditorTopBar() {
             onClick={runCode}
           >
             <Play className="w-4 h-4" />
+          </Button>
+        </Tooltip>
+        <Tooltip content="Toogle layout">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="w-8 h-8"
+            style={{
+              color: currentTheme.ui.foreground,
+            } as React.CSSProperties}
+            onClick={toogleLayout}
+          >
+            {layout === 'horizontal' ? <Columns className="size-4" /> : <Rows className="size-4" />}
           </Button>
         </Tooltip>
         <EditorSettingsDialog />
