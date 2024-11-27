3ef353a76b789c9c1ac6faaa1ab24161b0399347
ACBC <acbc.dev@gmail.com>
2024-11-27 17:53:49 -0500

    change seo to the index html

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { CodeEditor } from '@/components/code-editor/code-editor'
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { Toaster } from "@/components/ui/toaster"
-import SEO from './components/SEO'
 
 function App() {
   return (
     <>
-      <SEO title='RunTS - TypeScript Playground' />
       <TooltipProvider>
         <CodeEditor />
         <Toaster />
--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { HelmetProvider } from 'react-helmet-async';
+
 import { AptabaseProvider } from '@aptabase/react';
 
 // console.log(import.meta.env)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AptabaseProvider appKey={import.meta.env.VITE_APTABASE_KEY}>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
+      <App />
     </AptabaseProvider>
   </React.StrictMode>,
 )
