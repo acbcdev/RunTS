29afa3351cc5dc8f79ed406bb604990787aa7cba
ACBC <acbc.dev@gmail.com>
2024-11-27 18:12:51 -0500

    delete analithic

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import { AptabaseProvider } from '@aptabase/react';
-
 // console.log(import.meta.env)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AptabaseProvider appKey={import.meta.env.VITE_APTABASE_KEY}>
-      <App />
-    </AptabaseProvider>
+    <App />
   </React.StrictMode>,
 )
