330b0babd13dee6e69746254d171f4673a7ee903
ACBC <acbc.dev@gmail.com>
2024-12-19 15:41:45 -0500

    enhance PWA support with VitePWA integration, add Cloudflare adapter, and update CodeEditor for URL code sharing

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,16 +2,72 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import path from "node:path";
-// https://astro.build/config
+import { VitePWA } from "vite-plugin-pwa";
+import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
-	site: "runts.acbc.dev",
-	integrations: [react(), tailwind()],
-	vite: {
-		resolve: {
-			alias: {
-				"@core": path.resolve("../core/src"),
-				"@/web": "./src",
-			},
-		},
+  site: "https://runts.acbc.dev",
+  integrations: [react(), tailwind()],
+
+  devToolbar: {
+      enabled: false,
 	},
+
+  output: "server",
+
+  vite: {
+      plugins: [
+          VitePWA({
+              registerType: "autoUpdate",
+              strategies: "generateSW",
+              manifest: {
+                  name: "Runts",
+                  short_name: "Runts",
+                  description: "Typescript playground with themes ",
+                  theme_color: "#000000",
+                  icons: [
+                      {
+                          src: "/icons/pwa-512x512.png",
+                          sizes: "512x512",
+                          type: "image/png",
+                          purpose: "any",
+                      },
+                      {
+                          src: "/icons/pwa-192x192.png",
+                          sizes: "192x192",
+                          type: "image/png",
+                          purpose: "any",
+                      },
+                      {
+                          src: "/icons/pwa-maskable-192x192.png",
+                          sizes: "192x192",
+                          type: "image/png",
+                          purpose: "maskable",
+                      },
+                      {
+                          src: "/icons/pwa-maskable-512x512.png",
+                          sizes: "512x512",
+                          type: "image/png",
+                          purpose: "maskable",
+                      },
+                  ],
+              },
+              devOptions: {
+                  enabled: true,
+              },
+              workbox: {
+                  maximumFileSizeToCacheInBytes: 1024 * 1024 * 5,
+                  globPatterns: ["**/*.{js,css,html,png,svg,woff2}"],
+                  cleanupOutdatedCaches: true,
+              },
+          }),
+      ],
+      resolve: {
+          alias: {
+              "@core": path.resolve("../core/src"),
+              "@/web": "./src",
+          },
+      },
+	},
+
+  adapter: cloudflare(),
 });
