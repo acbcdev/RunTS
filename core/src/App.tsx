e933b6b1937d64e1e06ab7293ea1843b3fbd12c2
ACBC <acbc.dev@gmail.com>
2024-12-20 12:11:24 -0500

    fix: add error handling for code decoding in App component and display toast notification

--- core/src/App.tsx
+++ core/src/App.tsx
@@ -3,15 +3,26 @@ import { TooltipProvider } from "@core/components/ui/tooltip";
 import { Toaster } from "@core/components/ui/toaster";
 import { useEditorStore } from "./store/editor";
 import { useEffect } from "react";
+import { useToast } from "@core/hooks/use-toast";
 
 function App({ code }: { code?: string | null }) {
   const { addTab, setActiveTab } = useEditorStore()
+  const { toast } = useToast()
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
     if (code) {
-      const codigo = atob(code)
-      const id = addTab({ name: "shared.ts", language: "typescript", code: codigo, logs: [], logFormated: "" })
-      setActiveTab(id)
+      try {
+        const codigo = atob(code)
+        const id = addTab({ name: "shared.ts", language: "typescript", code: codigo, logs: [], logFormated: "" })
+        setActiveTab(id)
+
+      } catch {
+        toast({
+          variant: "destructive",
+          title: "Invalid Code",
+          description: "The code is error on decode",
+        })
+      }
     }
   }, [])
   return (
