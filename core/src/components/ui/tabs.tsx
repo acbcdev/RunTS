78dcb95436668cd8bb83901111944161e57d6ac6
ACBC <acbc.dev@gmail.com>
2024-12-20 11:51:03 -0500

    refactor: update ResizableHandle and TabsTrigger styles for improved hover effects and consistency

--- core/src/components/ui/tabs.tsx
+++ core/src/components/ui/tabs.tsx
@@ -40,7 +40,7 @@ const TabsTrigger = React.forwardRef<
     <TabsPrimitive.Trigger
       ref={ref}
       className={cn(
-        "inline-flex items-center justify-center whitespace-nowrap  px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 duration-150 border border-transparent data-[state=active]:border-b-zinc-500 mx-2",
+        "inline-flex items-center justify-center whitespace-nowrap  px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 duration-150 border rounded border-transparent data-[state=active]:border-zinc-500  mx-2",
         className,
       )}
       style={
