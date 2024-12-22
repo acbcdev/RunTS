a37e6c90b65f0a21ab5202f89b4044d2aa74c707
ACBC <acbc.dev@gmail.com>
2024-12-21 20:59:29 -0500

    feat: integrate PWA support with vite-plugin-pwa and update configurations

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,9 +3,57 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import path from "node:path";
 import cloudflare from "@astrojs/cloudflare";
+import AstroPWA from "@vite-pwa/astro";
 export default defineConfig({
   site: "https://runts.acbc.dev",
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind(),
+    AstroPWA({
+      registerType: "autoUpdate",
+      strategies: "generateSW",
+      manifest: {
+        name: "Runts",
+        short_name: "Runts",
+        description: "Typescript playground with themes ",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/icons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/icons/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icons/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 1024 * 1024 * 5,
+        globPatterns: ["**/*.{js,css,html,png,svg,woff2}"],
+        cleanupOutdatedCaches: true,
+      },
+    }),
+  ],
 
   devToolbar: {
     enabled: false,
