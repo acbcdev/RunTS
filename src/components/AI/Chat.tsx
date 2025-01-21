e3611bf19d0f34e5411add893f1cf0fe9f3db2ce
acbc <acbc.dev@gmail.com>
2025-01-21 12:16:06 -0500

    refactor: replace chat input with MultiModalInput component for improved functionality

--- src/components/AI/Chat.tsx
+++ src/components/AI/Chat.tsx
@@ -1,29 +1,13 @@
-import { Button } from "@/components/ui/button";
-import { Send, StopCircle } from "lucide-react";
 import remarkGfm from "remark-gfm";
-import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectLabel,
-	SelectTrigger,
-	SelectValue,
-} from "@/components/ui/select";
-import { useAIConfigStore } from "@/store/aiConfig";
 import ReactMarkdown from "react-markdown";
 import { ScrollArea } from "../ui/scroll-area";
-import { SelectGroup } from "@radix-ui/react-select";
-import TextareaAutosize from "../ui/TextareaAutosize";
-import type { providers } from "@/types/ai";
 import { useChat } from "@/hooks/useChat";
+import MultiModalInput from "./core/MultiModal-Input";
+
+import { useAIConfigStore } from "@/store/aiConfig";
+import { AI } from "../settings/tabs/ai";
 export function Chat() {
-	const {
-		provider,
-		getProviders,
-		setProvider,
-		selectedModel,
-		setSelectedModel,
-	} = useAIConfigStore();
+	const { getProviders } = useAIConfigStore();
 	const {
 		input,
 		messages,
@@ -32,7 +16,10 @@ export function Chat() {
 		streamingContent,
 		handleSubmit,
 	} = useChat();
-	return (
+
+	return getProviders().length === 0 ? (
+		<AI tabs={false} />
+	) : (
 		<aside className="relative flex flex-col w-full h-full max-w-2xl border-none rounded-none shadow-none bg-background">
 			<ScrollArea className="flex-1 h-0">
 				<section className="p-4 space-y-4 overflow-y-auto">
@@ -71,76 +58,12 @@ export function Chat() {
 			</ScrollArea>
 
 			<section className="left-0 right-0 flex flex-col gap-2 p-4 bg-background">
-				<form
-					onSubmit={handleSubmit}
-					className="flex w-full gap-2 mx-auto border rounded-lg bg-background border-border"
-				>
-					<div className="flex flex-col flex-1 gap-2 px-3 py-2">
-						<div className="flex flex-1">
-							<TextareaAutosize
-								value={input}
-								disabled={isLoading}
-								onChangeValue={(e) => setInput(e)}
-								placeholder="Message..."
-								onKeyDown={(event) => {
-									const key = event.key;
-									if (key === "Enter" && !event.shiftKey) {
-										event.preventDefault();
-										handleSubmit(event);
-									}
-									if (key === "Enter" && event.shiftKey) {
-										event.preventDefault();
-										setInput((prev) => `${prev}\n`);
-									}
-								}}
-								className="flex-1 break-words border-none shadow-none resize-none caret-accent focus:outline-none focus-visible:ring-0 focus:ring-0"
-							/>
-							<Button
-								disabled={isLoading}
-								type="submit"
-								size="icon"
-								variant={"ghost"}
-								className="size-5"
-							>
-								{isLoading ? <StopCircle /> : <Send />}
-							</Button>
-						</div>
-
-						<div className="flex w-fit ">
-							<Select
-								required
-								defaultValue={`${provider}::::${selectedModel}`}
-								onValueChange={(value) => {
-									// alert(value);
-									const [name, model] = value.split("::::");
-									setSelectedModel(model);
-									setProvider(name as providers);
-								}}
-							>
-								<SelectTrigger>
-									<SelectValue placeholder="Select model" />
-								</SelectTrigger>
-								<SelectContent>
-									{getProviders().map((provider) => (
-										<SelectGroup key={provider.name}>
-											<SelectLabel className="capitalize">
-												{provider.name}
-											</SelectLabel>
-											{provider.models.map((model) => (
-												<SelectItem
-													key={model}
-													value={`${provider.name}::::${model}`}
-												>
-													{model}
-												</SelectItem>
-											))}
-										</SelectGroup>
-									))}
-								</SelectContent>
-							</Select>
-						</div>
-					</div>
-				</form>
+				<MultiModalInput
+					input={input}
+					isLoading={isLoading}
+					handleSubmit={handleSubmit}
+					setInput={setInput}
+				/>
 			</section>
 		</aside>
 	);
--- src/components/AI/core/MultiModal-Input.tsx
+++ src/components/AI/core/MultiModal-Input.tsx
@@ -1,7 +1,105 @@
-import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import {
+	Select,
+	SelectTrigger,
+	SelectLabel,
+	SelectGroup,
+	SelectItem,
+	SelectContent,
+	SelectValue,
+} from "@/components/ui/select";
+import TextareaAutosize from "@/components/ui/TextareaAutosize";
+import { useAIConfigStore } from "@/store/aiConfig";
+import type { providers } from "@/types/ai";
+import { Send, StopCircle } from "lucide-react";
+import type { FormEvent } from "react";
+type MultiModalInputProps = {
+	input: string;
+	isLoading: boolean;
+	handleSubmit: (event: FormEvent) => void;
+	setInput: (value: string) => void;
+};
+export default function MultiModalInput({
+	input,
+	isLoading,
+	handleSubmit,
+	setInput,
+}: MultiModalInputProps) {
+	const {
+		provider,
+		getProviders,
+		setProvider,
+		selectedModel,
+		setSelectedModel,
+	} = useAIConfigStore();
+	return (
+		<form
+			onSubmit={handleSubmit}
+			className="flex w-full gap-2 mx-auto border rounded-lg bg-background border-border"
+		>
+			<div className="flex flex-col flex-1 gap-2 px-3 py-2">
+				<div className="flex flex-1">
+					<TextareaAutosize
+						value={input}
+						disabled={isLoading}
+						onChangeValue={(e) => setInput(e)}
+						placeholder="Message..."
+						onKeyDown={(event) => {
+							const key = event.key;
+							if (key === "Enter" && !event.shiftKey) {
+								event.preventDefault();
+								handleSubmit(event);
+							}
+							if (key === "Enter" && event.shiftKey) {
+								event.preventDefault();
+								setInput(`${input}\n`);
+							}
+						}}
+						className="flex-1 break-words border-none shadow-none resize-none caret-accent focus:outline-none focus-visible:ring-0 focus:ring-0"
+					/>
+					<Button
+						disabled={isLoading}
+						type="submit"
+						size="icon"
+						variant={"ghost"}
+						className="size-5"
+					>
+						{isLoading ? <StopCircle /> : <Send />}
+					</Button>
+				</div>
 
-export default function MultiModalInput() {
-  return (
-    <Textarea />
-  );
+				<div className="flex w-fit ">
+					<Select
+						required
+						defaultValue={`${provider}::::${selectedModel}`}
+						onValueChange={(value) => {
+							// alert(value);
+							const [name, model] = value.split("::::");
+							setSelectedModel(model);
+							setProvider(name as providers);
+						}}
+					>
+						<SelectTrigger>
+							<SelectValue placeholder="Select model" />
+						</SelectTrigger>
+						<SelectContent>
+							{getProviders().map(({ name, Icon, models }) => (
+								<SelectGroup key={name}>
+									<SelectLabel className="flex capitalize gap-x-2">
+										<Icon className="size-4" /> {name}
+									</SelectLabel>
+									{models.map((model) => (
+										<SelectItem key={model} value={`${name}::::${model}`}>
+											{model}
+										</SelectItem>
+									))}
+								</SelectGroup>
+							))}
+						</SelectContent>
+					</Select>
+					<span className="p-2">Beta</span>
+				</div>
+			</div>
+		</form>
+	);
 }
