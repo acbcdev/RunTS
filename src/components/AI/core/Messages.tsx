c52cd7cde5e09383dacbcc609c131b67fef8cc20
acbc <acbc.dev@gmail.com>
2025-01-31 20:04:43 -0500

    feat: implement Messages component for rendering chat messages with scrolling support

---
+++ src/components/AI/core/Messages.tsx
@@ -0,0 +1,57 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import type { Message } from "ai";
+import Markdown from "@/components/AI/core/Markdown";
+import { useScrollToBottom } from "@/hooks/useScrollToBottom";
+import { memo } from "react";
+
+function PureMessages({
+	messages,
+	streamingContent,
+}: { messages: Message[]; streamingContent: string }) {
+	const [containerRef, endRef] = useScrollToBottom<HTMLDivElement>();
+	return (
+		<ScrollArea ref={containerRef} className="flex-1 h-0 scroll-m-2 ">
+			<section className="px-4 py-2 space-y-4 overflow-auto">
+				{messages.map((message) => (
+					<div
+						key={message.id}
+						className={`flex ${
+							message.role === "user" ? "justify-end" : "justify-start"
+						}`}
+					>
+						<div
+							className={`rounded-lg p-4  ${
+								message.role === "user"
+									? "bg-border text-primary-foreground"
+									: "bg-input"
+							}`}
+						>
+							<Markdown>{message.content}</Markdown>
+						</div>
+					</div>
+				))}
+
+				<div
+					className="flex flex-col mb-4 prose break-all animate-pulse gap-x-2 hyphens-auto "
+					ref={endRef}
+				>
+					<Markdown
+					// className="prose break-all hyphens-auto dark:prose-invert "
+					>
+						{streamingContent}
+					</Markdown>
+					{streamingContent && (
+						<div className="rounded-full size-5 bg-border " />
+					)}
+				</div>
+			</section>
+		</ScrollArea>
+	);
+}
+
+// export const Messages = memo(PureMessages, (prevProps, nextProps) => {
+// if (prevProps.messages.length === nextProps.messages.length) return false;
+// 	return true;
+// });
+
+export const Messages = memo(PureMessages);
