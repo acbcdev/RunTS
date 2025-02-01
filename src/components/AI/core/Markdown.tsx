9affd574536cfbd84d74760003f36dbc4196a87d
acbc <acbc.dev@gmail.com>
2025-01-31 20:04:35 -0500

    refactor: enhance Markdown component styling and improve readability of code elements

--- src/components/AI/core/Markdown.tsx
+++ src/components/AI/core/Markdown.tsx
@@ -4,9 +4,10 @@ import ReactMarkdown, { type Components } from "react-markdown";
 import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import remarkGfm from "remark-gfm";
-// import { Editor } from "@monaco-editor/react";
+// import { Editor, OnMount } from "@monaco-editor/react";
 // import { useApparenceStore } from "@/store/apparence";
 // import { useShallow } from "zustand/react/shallow";
+// import { useRef } from "react";
 const components = {
 	code: ({ className, children }) => {
 		const match = /language-(\w+)/.exec(className || "");
@@ -16,67 +17,104 @@ const components = {
 			navigator.clipboard.writeText(codeContent);
 		};
 		console.log(match);
-		if (match) {
-			return (
-				<div className="relative max-w-2xl overflow-x-auto group/code">
-					<header className="sticky flex items-center justify-between px-4 py-2 border-b rounded-t-lg top-3 bg-[#282c34]">
-						<span className="capitalize">{match[1]}</span>
-						<nav className="top-0 right-0 z-10 opacity-0 group-hover/code:opacity-100">
-							<Button variant={"ghost"} onClick={copyToClipboard}>
-								<Copy />
-							</Button>
-						</nav>
-					</header>
 
-					<SyntaxHighlighter
-						language={match[1]}
-						style={oneDark}
-						PreTag="div"
-						codeTagProps={{
-							className: "whitespace-pre-wrap",
-							style: {
-								display: "inline-block",
-								minWidth: "100%",
-							},
-						}}
-					>
-						{codeContent}
-					</SyntaxHighlighter>
-				</div>
-			);
-		}
+		return match ? (
+			<div className="relative max-w-2xl overflow-x-auto group/code">
+				<header className="sticky flex items-center justify-between px-4 py-2 border-b rounded-t-lg top-3 bg-[#282c34]">
+					<span className="capitalize">{match[1]}</span>
+					<nav className="top-0 right-0 z-10 opacity-0 group-hover/code:opacity-100">
+						<Button variant={"ghost"} onClick={copyToClipboard}>
+							<Copy />
+						</Button>
+					</nav>
+				</header>
+
+				<SyntaxHighlighter
+					language={match[1]}
+					style={oneDark}
+					PreTag="div"
+					codeTagProps={{
+						className: "whitespace-pre-wrap",
+						style: {
+							display: "inline-block",
+							minWidth: "100%",
+						},
+					}}
+				>
+					{codeContent}
+				</SyntaxHighlighter>
+			</div>
+		) : (
+			<code className="bg-background/60  text-foreground px-2 py-1  rounded">
+				{children}
+			</code>
+		);
 	},
 	// code: ({ className, children }) => {
 	// 	const theme = useApparenceStore(useShallow((state) => state.theme));
 	// 	const match = /language-(\w+)/.exec(className || "");
 	// 	const height = String(children).split("\n").length * 20;
+	// 	const codeContent = String(children).replace(/\n$/, "");
+	// 	const copyToClipboard = () => {
+	// 		navigator.clipboard.writeText(codeContent);
+	// 	};
+	// 	const lang = ["tsx", "jsx"].includes(match?.[1] ?? "")
+	// 		? "javascript"
+	// 		: match?.[1];
+	// 	// Calcula el número de líneas
+
 	// 	return match ? (
-	// 		<Editor
-	// 			language={match[1] === "jsx" ? "javascript" : match[1]}
-	// 			height={height}
-	// 			theme={theme}
-	// 			value={String(children)}
-	// 			className="rounded-lg"
-	// 			options={{
-	// 				readOnly: true,
-	// 				renderLineHighlight: "none",
-	// 				minimap: {
-	// 					enabled: false,
-	// 				},
-	//        automaticLayout: true,
-	//        scrollBeyondLastLine:false
-	// 				scrollbar: {
-	// 					scrollByPage: false,
-	// 					horizontal: "hidden",
-	// 					vertical: "hidden",
-	// 				},
-	// 			}}
-	// 		/>
+	// 		<section className=" overflow-hidden  my-2  rounded-sm ">
+	// 			<header className="flex z-50 items-center justify-between bg-header py-1 px-2">
+	// 				<span className="px-5 text-accent">{match[1]}</span>
+	// 				<nav className=" ">
+	// 					<Button variant={"ghost"} size={"sm"} onClick={copyToClipboard}>
+	// 						<Copy />
+	// 					</Button>
+	// 				</nav>
+	// 			</header>
+	// 			<div className="relative">
+	// 				<div className="absolute inset-0 z-10 " />
+	// 				<Editor
+	// 					language={lang}
+	// 					height={height}
+	// 					theme={theme}
+	// 					value={String(children)}
+	// 					options={{
+	// 						readOnly: true,
+	// 						renderLineHighlight: "none",
+	// 						minimap: {
+	// 							enabled: false,
+	// 						},
+	// 						wordWrap: "on",
+	// 						lineNumbers: "off",
+	// 						lineNumbersMinChars: 3,
+	// 						overviewRulerLanes: 0,
+	// 						automaticLayout: true,
+	// 						scrollBeyondLastLine: false,
+	// 						scrollbar: {
+	// 							// scrollByPage: false,
+	// 							horizontal: "auto",
+	// 							vertical: "hidden",
+	// 						},
+	// 					}}
+	// 				/>
+	// 			</div>
+	// 		</section>
 	// 	) : (
-	// 		<code>{children}</code>
+	// 		<code className="bg-background/60  text-foreground px-2 py-1  rounded-sm">
+	// 			{children}
+	// 		</code>
 	// 	);
 	// },
 	pre: ({ children }) => <>{children}</>,
+	p: ({ node, children, ...props }) => {
+		return (
+			<p className="my-2 leading-7 " {...props}>
+				{children}
+			</p>
+		);
+	},
 	ol: ({ node, children, ...props }) => {
 		return (
 			<ol className="list-decimal list-outside ml-4" {...props}>
@@ -86,14 +124,14 @@ const components = {
 	},
 	li: ({ node, children, ...props }) => {
 		return (
-			<li className="py-1" {...props}>
+			<li className="py-1 my-2 leading-7" {...props}>
 				{children}
 			</li>
 		);
 	},
 	ul: ({ node, children, ...props }) => {
 		return (
-			<ul className="list-disc list-outside ml-4" {...props}>
+			<ul className="list-disc my-2 list-outside ml-4" {...props}>
 				{children}
 			</ul>
 		);
@@ -117,28 +155,28 @@ const components = {
 	),
 	h1: ({ node, children, ...props }) => {
 		return (
-			<h1 className="text-4xl font-semibold mt-6 mb-2" {...props}>
+			<h1 className="text-4xl font-black mt-6 mb-2" {...props}>
 				{children}
 			</h1>
 		);
 	},
 	h2: ({ node, children, ...props }) => {
 		return (
-			<h2 className="text-3xl font-semibold mt-6 mb-2" {...props}>
+			<h2 className="text-3xl font-black mt-6 mb-2" {...props}>
 				{children}
 			</h2>
 		);
 	},
 	h3: ({ node, children, ...props }) => {
 		return (
-			<h3 className="text-2xl font-semibold mt-6 mb-2" {...props}>
+			<h3 className="text-2xl font-black mt-6 mb-2" {...props}>
 				{children}
 			</h3>
 		);
 	},
 	h4: ({ node, children, ...props }) => {
 		return (
-			<h4 className="text-xl font-semibold mt-6 mb-2" {...props}>
+			<h4 className="text-xl font-bold mt-6 mb-2" {...props}>
 				{children}
 			</h4>
 		);
@@ -218,6 +256,9 @@ const components = {
 				{children}
 			</th>
 		);
+	},
+	hr: ({ node, children, ...props }) => {
+		return <hr className="my-4 border-muted" {...props} />;
 	},
 } satisfies Components;
 
