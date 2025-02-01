c035b9849e136a8d9d63c315872113ce7a7de35d
acbc <acbc.dev@gmail.com>
2025-01-31 20:04:16 -0500

    refactor: simplify Chat component by extracting message rendering to Messages component

--- src/components/AI/Chat.tsx
+++ src/components/AI/Chat.tsx
@@ -1,11 +1,10 @@
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { useChat } from "@/hooks/useChat";
 import MultiModalInput from "./core/MultiModal-Input";
 
-import Markdown from "@/components/AI/core/Markdown";
 import { AI } from "@/components/Settings/tabs/ai";
 import { useAIConfigStore } from "@/store/aiConfig";
 import { useShallow } from "zustand/react/shallow";
+import { Messages } from "./core/Messages";
 export function Chat() {
 	const { getProviders, showChat } = useAIConfigStore(
 		useShallow((state) => ({
@@ -27,42 +26,10 @@ export function Chat() {
 	) : (
 		<aside
 			data-state={showChat}
-			className="relative flex flex-col w-full h-full max-w-3xl mx-auto border-none rounded-none shadow-none chat bg-background"
+			className="relative flex flex-col w-full h-full max-w-3xl mx-auto border-none rounded-none shadow-none "
 		>
-			<ScrollArea className="flex-1 h-0 scroll-m-2 ">
-				<section className="p-4 space-y-4 overflow-auto">
-					{messages.map((message) => (
-						<div
-							key={message.id}
-							className={`flex ${
-								message.role === "user" ? "justify-end" : "justify-start"
-							}`}
-						>
-							<div
-								className={`rounded-lg p-4  ${
-									message.role === "user"
-										? "bg-border text-primary-foreground"
-										: "bg-input"
-								}`}
-							>
-								<Markdown>{message.content}</Markdown>
-							</div>
-						</div>
-					))}
-					{streamingContent && (
-						<div className="flex flex-col mb-4 prose break-all animate-pulse gap-x-2 hyphens-auto ">
-							<Markdown
-							// className="prose break-all hyphens-auto dark:prose-invert "
-							>
-								{streamingContent}
-							</Markdown>
-							<div className="rounded-full size-5 bg-border " />
-						</div>
-					)}
-				</section>
-			</ScrollArea>
-
-			<section className="left-0 right-0 flex flex-col gap-2 p-4 bg-background">
+			<Messages messages={messages} streamingContent={streamingContent} />
+			<section className=" flex flex-col gap-2 p-4 ">
 				<MultiModalInput
 					input={input}
 					isLoading={isLoading}
