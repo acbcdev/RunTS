9e12d76f140bc9cad0fb054e307084ddafab6f91
acbc <acbc.dev@gmail.com>
2025-02-02 15:27:36 -0500

    feat: add experimental console option to runCode for enhanced logging control

--- src/hooks/useRun.ts
+++ src/hooks/useRun.ts
@@ -4,40 +4,42 @@ import { useTabsStore } from "@/store/tabs";
 import { useShallow } from "zustand/react/shallow";
 
 export function useRun() {
-	const activeTab = useTabsStore(useShallow((state) => state.getCurrentTab()));
-	const updateTabLog = useTabsStore(useShallow((state) => state.updateTabLog));
-	const setRunning = useEditorStore(useShallow((state) => state.setRunning));
-
-	async function runCode() {
-		if (!activeTab) return;
-		if (!activeTab.code) {
-			updateTabLog(activeTab.id, []);
-		}
-		const loading = setTimeout(() => {
-			setRunning(true);
-		}, 500);
-		try {
-			const name = activeTab?.name;
-			const output = await runCodeWorker(activeTab.code, {
-				name,
-				injectLogs: true,
-			});
-			clearTimeout(loading);
-			updateTabLog(activeTab.id, output);
-		} catch (error) {
-			updateTabLog(activeTab.id, [
-				{
-					type: "error",
-					content: error instanceof Error ? error.message : "unknown error",
-					line: 0,
-					column: 0,
-					timestamp: Date.now(),
-				},
-			]);
-		} finally {
-			clearTimeout(loading);
-			setRunning(false);
-		}
-	}
-	return { runCode };
+  const activeTab = useTabsStore(useShallow((state) => state.getCurrentTab()));
+  const updateTabLog = useTabsStore(useShallow((state) => state.updateTabLog));
+  const setRunning = useEditorStore(useShallow((state) => state.setRunning));
+  const experimetalConsole = useEditorStore(
+    useShallow((state) => state.experimetalConsole)
+  );
+  async function runCode() {
+    if (!activeTab) return;
+    if (!activeTab.code) {
+      updateTabLog(activeTab.id, []);
+    }
+    const loading = setTimeout(() => {
+      setRunning(true);
+    }, 500);
+    try {
+      const name = activeTab?.name;
+      const output = await runCodeWorker(activeTab.code, {
+        name,
+        injectLogs: experimetalConsole,
+      });
+      clearTimeout(loading);
+      updateTabLog(activeTab.id, output);
+    } catch (error) {
+      updateTabLog(activeTab.id, [
+        {
+          type: "error",
+          content: error instanceof Error ? error.message : "unknown error",
+          line: 0,
+          column: 0,
+          timestamp: Date.now(),
+        },
+      ]);
+    } finally {
+      clearTimeout(loading);
+      setRunning(false);
+    }
+  }
+  return { runCode };
 }
