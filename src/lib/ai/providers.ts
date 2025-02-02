27a34c839ab9cee68099dcdc9fcde2b076913378
acbc <acbc.dev@gmail.com>
2025-02-02 15:56:22 -0500

    refactor: format code for consistency and remove commented-out provider models

--- src/lib/ai/providers.ts
+++ src/lib/ai/providers.ts
@@ -3,13 +3,15 @@ import { createAnthropic } from "@ai-sdk/anthropic";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { createOpenAI } from "@ai-sdk/openai";
 const generativeAIProviders = {
-	openai: createOpenAI,
-	google: createGoogleGenerativeAI,
-	anthropic: createAnthropic,
+  openai: createOpenAI,
+  google: createGoogleGenerativeAI,
+  anthropic: createAnthropic,
 };
 export function createProvider(provider: providers, apiKey: string) {
-	return generativeAIProviders[provider]({ apiKey });
+  return generativeAIProviders[provider]({ apiKey });
 }
+// google('')
+// openai('o3-mini')
 // openai("gpt-4o-2024-11-20");
 // openai("gpt-4o-mini-2024-07-18");
 // openai("o1-mini-2024-09-12");
@@ -21,27 +23,3 @@ export function createProvider(provider: providers, apiKey: string) {
 // google("gemini-2.0-flash-exp");
 // google("gemini-1.5-pro-latest");
 // google("gemini-1.5-flash-latest");
-
-// const providersList: GoogleGenerativeAIModelId[] = [
-//   // {
-//   //   name: "openai",
-//   //   models: [
-//   //     "gpt-4o-2024-11-20",
-//   //     "gpt-4o-mini-2024-07-18",
-//   //     "o1-mini-2024-09-12",
-//   //     "o1-2024-12-17",
-//   //   ],
-//   // },
-//   // {
-//   //   name: "anthropic",
-//   //   models: ["claude-3-sonnet-20240229", "claude-3-5-haiku-latest"],
-//   // },
-//   // {
-//   //   name: "google",
-//   //   models: [
-//   //     "gemini-2.0-flash-exp",
-//   //     "gemini-1.5-pro-latest",
-//   //     "gemini-1.5-flash-latest",
-//   //   ],
-//   // },
-// ];
