dc8f59c66e1d0a870c2dcc40ec0347abe28acf71
acbc <acbc.dev@gmail.com>
2025-02-02 10:39:49 -0500

    refactor: extract context menu item and tab name handling into separate components for improved readability

--- src/components/Tabs/EditorTabsItem.tsx
+++ src/components/Tabs/EditorTabsItem.tsx
@@ -4,6 +4,7 @@ import {
 	ContextMenuContent,
 	ContextMenuItem,
 	ContextMenuTrigger,
+	ContextMenuSeparator,
 } from "@/components/ui/context-menu";
 import { Input } from "@/components/ui/input";
 import { useHandler } from "@/hooks/useHandler";
@@ -16,23 +17,24 @@ import { useRef } from "react";
 import { toast } from "sonner";
 import { useShallow } from "zustand/react/shallow";
 
-export function EditorTabsItem({ tab }: { tab: Tab }) {
+const TabContextMenuItem = ({
+	children,
+	tabs,
+	tab,
+	removeTab,
+	setEditing,
+	inputRef,
+}: {
+	children: React.ReactNode;
+	tabs: Tab[];
+	tab: Tab;
+	removeTab: (id: string) => void;
+	setEditing: (id: string, value: boolean) => void;
+	inputRef: React.RefObject<HTMLInputElement>;
+}) => {
 	const { handleShare, copyCode, downloadCode } = useHandler();
-	const inputRef = useRef<HTMLInputElement | null>(null);
-	const editorRef = useEditorStore(useShallow((state) => state.editorRef));
-	const tabs = useTabsStore(useShallow((state) => state.tabs));
-	const activeTabId = useTabsStore(useShallow((state) => state.activeTabId));
-	const setActiveTab = useTabsStore(useShallow((state) => state.setActiveTab));
-	const setEditing = useTabsStore(useShallow((state) => state.setEditing));
-	const removeTab = useTabsStore(useShallow((state) => state.removeTab));
-	const addTab = useTabsStore(useShallow((state) => state.addTab));
-	const changeNameTab = useTabsStore(
-		useShallow((state) => state.changeNameTab),
-	);
 
-	const updateTabCode = useTabsStore(
-		useShallow((state) => state.updateTabCode),
-	);
+	const addTab = useTabsStore(useShallow((state) => state.addTab));
 
 	const handleDuplicateTab = (tabId: Tab["id"]) => {
 		const duplicateTab = tabs.find((tab) => tab.id === tabId);
@@ -46,93 +48,9 @@ export function EditorTabsItem({ tab }: { tab: Tab }) {
 		// handleActiveTabChange(id);
 	};
 
-	const handleActiveTabChange = (tabId: Tab["id"]) => {
-		setActiveTab(tabId);
-		updateTabCode(tabId, tabs.find((tab) => tab.id === tabId)?.code || "");
-		editorRef?.focus();
-	};
-
-	const handleTabEdited = (tabId: string) => {
-		const tab = tabs.find((tab) => tab.id === tabId);
-		if (!tab) return;
-		if (!tab.name.trim()) {
-			changeNameTab(tabId, "code.ts");
-			setEditing(tabId);
-			return;
-		}
-		const name = tab.name.trim();
-		const tsFile = name.endsWith(".ts") || name.endsWith(".js");
-		const [nameTab] = name.split(/\.$/);
-		let changeName = nameTab.replace(/\s+/g, "-");
-		if (!tsFile) changeName = `${changeName.slice(0, 20)}.ts`;
-		changeNameTab(activeTabId, changeName);
-		setEditing(tabId, false);
-		toast.success("Tab name changed", { duration: 700 });
-	};
-
 	return (
-		<ContextMenu key={tab.id}>
-			<ContextMenuTrigger>
-				{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-				<div
-					className={cn(
-						`${activeTabId === tab.id ? "bg-header " : "bg-transparent hover:bg-background"}`,
-						"  group/tab flex items-center gap-2 px-3 py-1.5 border-r cursor-pointer transition-colors  ",
-					)}
-					onClick={() => {
-						if (!tab.editing || activeTabId !== tab.id) {
-							handleActiveTabChange(tab.id);
-						}
-					}}
-				>
-					{tab.editing ? (
-						<Input
-							ref={inputRef}
-							value={tab.name}
-							onKeyDown={(e) => {
-								if (e.code === "Enter") {
-									e.preventDefault();
-									handleTabEdited(tab.id);
-								}
-							}}
-							className="border-none  rounded-none outline-none m-0  max-w-32 h-full p-0   focus-visible:ring-0"
-							onChange={(e) => changeNameTab(tab.id, e.target.value)}
-							onBlur={() => handleTabEdited(tab.id)}
-						/>
-					) : (
-						// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
-						<span
-							className={cn(`${tab.id === activeTabId && "underline"}  `)}
-							spellCheck="false"
-							onClick={() => {
-								if (activeTabId === tab.id) {
-									setEditing(tab.id, true);
-									setTimeout(() => {
-										inputRef.current?.focus();
-									}, 100);
-								}
-							}}
-						>
-							{tab.name}
-						</span>
-					)}
-					{tabs.length > 1 && (
-						<Button
-							variant="destructive"
-							size="icon"
-							aria-label="Close tab"
-							translate="no"
-							className="h-5 w-5 p-0 bg-transparent rounded-full"
-							onClick={(e) => {
-								e.stopPropagation();
-								removeTab(tab.id);
-							}}
-						>
-							<X className="size-3" />
-						</Button>
-					)}
-				</div>
-			</ContextMenuTrigger>
+		<ContextMenu>
+			<ContextMenuTrigger>{children}</ContextMenuTrigger>
 			<ContextMenuContent>
 				<ContextMenuItem
 					onClick={() => {
@@ -144,9 +62,11 @@ export function EditorTabsItem({ tab }: { tab: Tab }) {
 				>
 					Rename
 				</ContextMenuItem>
+
 				<ContextMenuItem onClick={() => handleDuplicateTab(tab.id)}>
 					Duplicate
 				</ContextMenuItem>
+				<ContextMenuSeparator />
 				<ContextMenuItem
 					onClick={() => {
 						copyCode(tab.id);
@@ -164,6 +84,7 @@ export function EditorTabsItem({ tab }: { tab: Tab }) {
 				<ContextMenuItem onClick={() => handleShare(tab.id)}>
 					Share
 				</ContextMenuItem>
+				<ContextMenuSeparator />
 				<ContextMenuItem
 					className="focus:bg-destructive focus:text-destructive-foreground"
 					onClick={() => removeTab(tab.id)}
@@ -172,5 +93,144 @@ export function EditorTabsItem({ tab }: { tab: Tab }) {
 				</ContextMenuItem>
 			</ContextMenuContent>
 		</ContextMenu>
+	);
+};
+
+const EditorTabItemName = ({
+	tab,
+	tabs,
+	inputRef,
+	changeNameTab,
+	activeTabId,
+	setEditing,
+}: {
+	tab: Tab;
+	tabs: Tab[];
+	inputRef: React.RefObject<HTMLInputElement>;
+	changeNameTab: (id: string, name: string) => void;
+	activeTabId: string;
+	setEditing: (id: string, value?: boolean) => void;
+}) => {
+	const handleTabEdited = (tabId: string) => {
+		const tab = tabs.find((tab) => tab.id === tabId);
+		if (!tab) return;
+		if (!tab.name.trim()) {
+			changeNameTab(tabId, "code.ts");
+			setEditing(tabId);
+			return;
+		}
+		const name = tab.name.trim();
+		const tsFile = name.endsWith(".ts") || name.endsWith(".js");
+		const [nameTab] = name.split(/\.$/);
+		let changeName = nameTab.replace(/\s+/g, "-");
+		if (!tsFile) changeName = `${changeName.slice(0, 20)}.ts`;
+		changeNameTab(activeTabId, changeName);
+		setEditing(tabId, false);
+		toast.success("Tab name changed", { duration: 700 });
+	};
+	return (
+		<>
+			{tab.editing ? (
+				<Input
+					ref={inputRef}
+					value={tab.name}
+					onKeyDown={(e) => {
+						if (e.code === "Enter") {
+							e.preventDefault();
+							handleTabEdited(tab.id);
+						}
+					}}
+					className="border-none  rounded-none outline-none m-0  max-w-32 h-full p-0   focus-visible:ring-0"
+					onChange={(e) => changeNameTab(tab.id, e.target.value)}
+					onBlur={() => handleTabEdited(tab.id)}
+				/>
+			) : (
+				// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+				<span
+					className={cn(`${tab.id === activeTabId && "underline"}  `)}
+					spellCheck="false"
+					onClick={() => {
+						if (activeTabId === tab.id) {
+							setEditing(tab.id, true);
+							setTimeout(() => {
+								inputRef.current?.focus();
+							}, 100);
+						}
+					}}
+				>
+					{tab.name}
+				</span>
+			)}
+		</>
+	);
+};
+
+export function EditorTabsItem({ tab }: { tab: Tab }) {
+	const inputRef = useRef<HTMLInputElement | null>(null);
+	const editorRef = useEditorStore(useShallow((state) => state.editorRef));
+	const tabs = useTabsStore(useShallow((state) => state.tabs));
+	const activeTabId = useTabsStore(useShallow((state) => state.activeTabId));
+	const setActiveTab = useTabsStore(useShallow((state) => state.setActiveTab));
+	const setEditing = useTabsStore(useShallow((state) => state.setEditing));
+	const removeTab = useTabsStore(useShallow((state) => state.removeTab));
+	const changeNameTab = useTabsStore(
+		useShallow((state) => state.changeNameTab),
+	);
+
+	const updateTabCode = useTabsStore(
+		useShallow((state) => state.updateTabCode),
+	);
+
+	const handleActiveTabChange = (tabId: Tab["id"]) => {
+		setActiveTab(tabId);
+		updateTabCode(tabId, tabs.find((tab) => tab.id === tabId)?.code || "");
+		editorRef?.focus();
+	};
+
+	return (
+		<TabContextMenuItem
+			tabs={tabs}
+			tab={tab}
+			removeTab={removeTab}
+			setEditing={setEditing}
+			inputRef={inputRef}
+		>
+			{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+			<div
+				className={cn(
+					`${activeTabId === tab.id ? "bg-header " : "bg-transparent hover:bg-background"}`,
+					"  group/tab flex items-center gap-2 px-3 py-1.5 border-r cursor-pointer transition-colors  ",
+				)}
+				onClick={() => {
+					if (!tab.editing || activeTabId !== tab.id) {
+						handleActiveTabChange(tab.id);
+					}
+				}}
+			>
+				<EditorTabItemName
+					tab={tab}
+					inputRef={inputRef}
+					tabs={tabs}
+					changeNameTab={changeNameTab}
+					activeTabId={activeTabId}
+					setEditing={setEditing}
+				/>
+				{tabs.length > 1 && (
+					<Button
+						variant="destructive"
+						size="icon"
+						aria-label="Close tab"
+						translate="no"
+						className="h-5 w-5 p-0 bg-transparent rounded-full"
+						onClick={(e) => {
+							e.stopPropagation();
+							removeTab(tab.id);
+						}}
+					>
+						<X className="size-3" />
+					</Button>
+				)}
+			</div>
+		</TabContextMenuItem>
 	);
 }
