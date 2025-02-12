79b64d5b47594155e2e4beb10efbde0fc2b62966
acbc <acbc.dev@gmail.com>
2025-02-12 12:55:05 -0500

    refactor: update TypeScript compiler options in EditorMain for improved configuration

--- src/components/Editor/EditorMain.tsx
+++ src/components/Editor/EditorMain.tsx
@@ -63,7 +63,13 @@ export function EditorMain() {
 					module: monacoInstance.languages.typescript.ModuleKind.ESNext,
 					allowNonTsExtensions: true,
 					lib: ["esnext", "dom"],
-
+					isolatedModules: true,
+					composite: false,
+					declaration: false,
+					allowJs: true,
+					checkJs: true,
+					skipLibCheck: true,
+					noResolve: true,
 					noEmit: true,
 				},
 			);
