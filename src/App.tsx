429bb548cbe28b1a7ab38e200b0b68f07d5efe77
acbc <acbc.dev@gmail.com>
2025-02-14 20:00:29 -0500

    fix: add contributor information and project details to console log

--- src/App.tsx
+++ src/App.tsx
@@ -8,39 +8,51 @@ import { useShallow } from "zustand/react/shallow";
 import { decode } from "js-base64";
 
 function App() {
-	const { addTab, setActiveTab } = useTabsStore(useShallow((state) => state));
-	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-	useEffect(() => {
-		const code = new URLSearchParams(window.location.search).get("code");
-		window.history.replaceState(null, "", "/");
-		if (code) {
-			try {
-				const decodedCode = decode(code);
-				const id = addTab({
-					name: "shared.ts",
-					language: "typescript",
-					code: decodedCode,
-					logs: [],
-				});
-				setActiveTab(id);
-				toast.success("Code loaded", {
-					description: "The code has been loaded.",
-					duration: 2000,
-				});
-			} catch {
-				toast.error("invaliad Code", {
-					description: "The code is error on decode",
-				});
-			}
-		}
-	}, []);
-	return (
-		<>
-			<TooltipProvider delayDuration={500} skipDelayDuration={100}>
-				<CodeEditor />
-				<Toaster />
-			</TooltipProvider>
-		</>
-	);
+  const { addTab, setActiveTab } = useTabsStore(useShallow((state) => state));
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    console.log(`
+		
+		open to contributors ❤️
+		
+		repo:	https://github.com/acbcdev/Runts 
+		author:	https://acbc.dev
+		license:	Apache-2.0
+
+		This is a JavaScript/TypeScript code runner.
+
+
+			`);
+    const code = new URLSearchParams(window.location.search).get("code");
+    window.history.replaceState(null, "", "/");
+    if (code) {
+      try {
+        const decodedCode = decode(code);
+        const id = addTab({
+          name: "shared.ts",
+          language: "typescript",
+          code: decodedCode,
+          logs: [],
+        });
+        setActiveTab(id);
+        toast.success("Code loaded", {
+          description: "The code has been loaded.",
+          duration: 2000,
+        });
+      } catch {
+        toast.error("invaliad Code", {
+          description: "The code is error on decode",
+        });
+      }
+    }
+  }, []);
+  return (
+    <>
+      <TooltipProvider delayDuration={500} skipDelayDuration={100}>
+        <CodeEditor />
+        <Toaster />
+      </TooltipProvider>
+    </>
+  );
 }
 export default App;
