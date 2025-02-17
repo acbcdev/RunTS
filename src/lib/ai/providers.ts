8c1fb162925f13a1bc551c9b2e6e680e65093f25
acbc <acbc.dev@gmail.com>
2025-02-17 18:33:36 -0500

    fix: improve code formatting and consistency in providers.ts

--- src/lib/ai/providers.ts
+++ src/lib/ai/providers.ts
@@ -3,23 +3,10 @@ import { createAnthropic } from "@ai-sdk/anthropic";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { createOpenAI } from "@ai-sdk/openai";
 const generativeAIProviders = {
-	openai: createOpenAI,
-	google: createGoogleGenerativeAI,
-	anthropic: createAnthropic,
+  openai: createOpenAI,
+  google: createGoogleGenerativeAI,
+  anthropic: createAnthropic,
 };
 export function createProvider(provider: providers, apiKey: string) {
-	return generativeAIProviders[provider]({ apiKey });
+  return generativeAIProviders[provider]({ apiKey });
 }
-// google('')
-// openai('o3-mini')
-// openai("gpt-4o-2024-11-20");
-// openai("gpt-4o-mini-2024-07-18");
-// openai("o1-mini-2024-09-12");
-// openai("o1-2024-12-17");
-
-// anthropic("claude-3-sonnet-20240229");
-// anthropic("claude-3-5-haiku-latest");
-
-// google("gemini-2.0-flash-exp");
-// google("gemini-1.5-pro-latest");
-// google("gemini-1.5-flash-latest");
