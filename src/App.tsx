ee3ed37b9c981970dbde5f4ae0fdbbda46f719ef
acbc <acbc.dev@gmail.com>
2025-02-16 21:09:32 -0500

    fix: improve code formatting and consistency in App component

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ import { useShallow } from "zustand/react/shallow";
 import { decode } from "js-base64";
 
 function App() {
-  const { addTab, setActiveTab } = useTabsStore(useShallow((state) => state));
-  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-  useEffect(() => {
-    console.log(`
+	const { addTab, setActiveTab } = useTabsStore(useShallow((state) => state));
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		console.log(`
 		
 		open to contributors ❤️
 		
@@ -23,36 +23,36 @@ function App() {
 
 
 			`);
-    const code = new URLSearchParams(window.location.search).get("code");
-    window.history.replaceState(null, "", "/");
-    if (code) {
-      try {
-        const decodedCode = decode(code);
-        const id = addTab({
-          name: "shared.ts",
-          language: "typescript",
-          code: decodedCode,
-          logs: [],
-        });
-        setActiveTab(id);
-        toast.success("Code loaded", {
-          description: "The code has been loaded.",
-          duration: 2000,
-        });
-      } catch {
-        toast.error("invaliad Code", {
-          description: "The code is error on decode",
-        });
-      }
-    }
-  }, []);
-  return (
-    <>
-      <TooltipProvider delayDuration={500} skipDelayDuration={100}>
-        <CodeEditor />
-        <Toaster />
-      </TooltipProvider>
-    </>
-  );
+		const code = new URLSearchParams(window.location.search).get("code");
+		window.history.replaceState(null, "", "/");
+		if (code) {
+			try {
+				const decodedCode = decode(code);
+				const id = addTab({
+					name: "shared.ts",
+					language: "typescript",
+					code: decodedCode,
+					logs: [],
+				});
+				setActiveTab(id);
+				toast.success("Code loaded", {
+					description: "The code has been loaded.",
+					duration: 2000,
+				});
+			} catch {
+				toast.error("invaliad Code", {
+					description: "The code is error on decode",
+				});
+			}
+		}
+	}, []);
+	return (
+		<>
+			<TooltipProvider delayDuration={500} skipDelayDuration={100}>
+				<CodeEditor />
+				<Toaster />
+			</TooltipProvider>
+		</>
+	);
 }
 export default App;
