b778cf794ece7cb4d7684a916325fb9b6ac0fc0e
acbc <acbc.dev@gmail.com>
2025-02-19 20:09:17 -0500

    refactor: improve ReloadPrompt component styling and structure

--- src/components/ReloadPrompt.tsx
+++ src/components/ReloadPrompt.tsx
@@ -1,48 +1,48 @@
 import { useRegisterSW } from "virtual:pwa-register/react";
 import { Button } from "@/components/ui/button";
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { Info, RefreshCw } from "lucide-react";
 import { toast } from "sonner";
 
 export function ReloadPrompt() {
-	const {
-		needRefresh: [needRefresh],
-		updateServiceWorker,
-	} = useRegisterSW({
-		onRegisteredSW() {
-			console.log("Service Worker Registered");
-		},
-		onRegisterError() {
-			console.log("SW registration error");
-		},
-		onOfflineReady() {
-			toast("App ready to work offline");
-		},
-	});
+  const {
+    needRefresh: [needRefresh],
+    updateServiceWorker,
+  } = useRegisterSW({
+    onRegisteredSW() {
+      console.log("Service Worker Registered");
+    },
+    onRegisterError() {
+      console.log("SW registration error");
+    },
+    onOfflineReady() {
+      toast("App ready to work offline");
+    },
+  });
 
-	return (
-		<>
-			{needRefresh && (
-				<Card className="fixed bottom-2 right-1.5 border-accent animate-in delay-700">
-					<CardHeader>
-						<CardTitle className="flex items-center gap-x-1.5">
-							<Info className="text-accent" /> New content available
-						</CardTitle>
-						<CardDescription>click on reload button to update.</CardDescription>
-					</CardHeader>
-					<CardContent className="flex items-center gap-x-2 justify-center">
-						<Button className="w-fit" onClick={() => updateServiceWorker(true)}>
-							<RefreshCw /> Reload
-						</Button>
-					</CardContent>
-				</Card>
-			)}
-		</>
-	);
+  return (
+    <>
+      {needRefresh && (
+        <Card className="fixed z-[1000] bottom-2 right-1.5 border-accent animate-in delay-700">
+          <CardHeader>
+            <CardTitle className="flex items-center gap-x-1.5">
+              <Info className="text-accent" /> New content available
+            </CardTitle>
+            <CardDescription>click on reload button to update.</CardDescription>
+          </CardHeader>
+          <CardContent className="flex items-center gap-x-2 justify-center">
+            <Button className="w-fit" onClick={() => updateServiceWorker(true)}>
+              <RefreshCw /> Reload
+            </Button>
+          </CardContent>
+        </Card>
+      )}
+    </>
+  );
 }
