a0aaa7d6260a93e93fa6e8079786ee4637901e25
acbc <acbc.dev@gmail.com>
2025-03-01 18:32:33 -0500

    refactor: update Formatter function to use options object for depth management

--- src/lib/formatter.ts
+++ src/lib/formatter.ts
@@ -1,21 +1,30 @@
-export function Formatter<T>(item: T, depth = 5, currentDepth = 0): string {
-	if (currentDepth > depth) return "...";
+type FormatterOptions = {
+	depth: number;
+	currentDepth: number;
+};
+export function Formatter<T>(
+	item: T,
+	options: FormatterOptions = { depth: 5, currentDepth: 0 },
+): string {
+	if (options.currentDepth > options.depth) return "...";
 
 	if (Array.isArray(item)) {
-		return `[ ${item.map((el) => Formatter(el, depth, currentDepth + 1)).join(", ")} ]`;
+		return `[ ${item.map((el) => Formatter(el, { depth: options.depth, currentDepth: options.currentDepth + 1 })).join(", ")} ]`;
 	}
-
+	if (typeof item === "function") {
+		return `ƒ function ${item.name || "anonymous"}()`;
+	}
 	if (item !== null && typeof item === "object") {
 		if (item instanceof Map) {
 			return `Map (${item.size}) {\n${[...item.entries()]
 				.map(
 					([key, value]) =>
-						`  ${Formatter(key, depth, currentDepth + 1)} => ${Formatter(value, depth, currentDepth + 1)}`,
+						`  ${Formatter(key, { depth: options.depth, currentDepth: options.currentDepth + 1 })} => ${Formatter(value, { depth: options.depth, currentDepth: options.currentDepth + 1 })}`,
 				)
 				.join(",\n")}\n}`;
 		}
 		if (item instanceof Set) {
-			return `Set(${item.size}) {\n  ${[...item.values()].map((value) => Formatter(value, depth, currentDepth + 1)).join(",\n  ")} \n}`;
+			return `Set(${item.size}) {\n  ${[...item.values()].map((value) => Formatter(value, { depth: options.depth, currentDepth: options.currentDepth + 1 })).join(",\n  ")} \n}`;
 		}
 		if (item instanceof Error) {
 			return `Error: ${item.name} - ${item.message}`;
@@ -24,7 +33,7 @@ export function Formatter<T>(item: T, depth = 5, currentDepth = 0): string {
 		const entries = Object.entries(item)
 			.map(
 				([key, value]) =>
-					`  ${key}: ${Formatter(value, depth, currentDepth + 1)}`,
+					`  ${key}: ${Formatter(value, { depth: options.depth, currentDepth: options.currentDepth + 1 })}`,
 			)
 			.join(",\n");
 		return `{\n${entries}\n}`;
