5faacb90fa165c0c42ab56dbb4efd7fa19a371ca
acbc <acbc.dev@gmail.com>
2025-08-14 22:54:23 -0500

    feat: refactor appearance and editor types, update radius handling, and improve code formatting

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,109 +1,115 @@
 import pg from "@/../package.json";
-import type { Radius } from "@/store/apparence";
 import { claude } from "@/svg/claude";
 import { gemini } from "@/svg/gemini";
 import mistral from "@/svg/mistral";
 import { openai } from "@/svg/openai";
 import type { model } from "@/types/ai";
+import type { Radius } from "@/types/editor";
 import type { lineRendererEditor } from "@/types/editor";
 import type { SVGProps } from "react";
 export const versionApp = pg.version;
 export const fontSizes = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32];
 
 export const refreshTimes = [
-	{ value: 0, time: "off" },
-	{ value: 100, time: "100ms" },
-	{ value: 200, time: "200ms" },
-	{ value: 300, time: "300ms" },
-	{ value: 400, time: "400ms" },
-	{ value: 500, time: "500ms" },
-	{ value: 700, time: "700ms" },
-	{ value: 800, time: "800ms" },
-	{ value: 1000, time: "1s" },
-	{ value: 2000, time: "2s" },
-	{ value: 3000, time: "3s" },
+  { value: 0, time: "off" },
+  { value: 100, time: "100ms" },
+  { value: 200, time: "200ms" },
+  { value: 300, time: "300ms" },
+  { value: 400, time: "400ms" },
+  { value: 500, time: "500ms" },
+  { value: 700, time: "700ms" },
+  { value: 800, time: "800ms" },
+  { value: 1000, time: "1s" },
+  { value: 2000, time: "2s" },
+  { value: 3000, time: "3s" },
 ];
 
 export const renderLines: lineRendererEditor[] = [
-	"none",
-	"gutter",
-	"line",
-	"all",
+  "none",
+  "gutter",
+  "line",
+  "all",
 ];
 export const fontFamilies = [
-	{ name: "Cascadia Code", value: '"Cascadia Code"' },
-	{ name: "Fira Code", value: '"Fira Code"' },
-	{ name: "Monocraft", value: "Monocraft" },
+  { name: "Cascadia Code", value: '"Cascadia Code"' },
+  { name: "Fira Code", value: '"Fira Code"' },
+  { name: "Monocraft", value: "Monocraft" },
 ];
 type TLayout = "vertical" | "horizontal";
 export const layouts: TLayout[] = ["vertical", "horizontal"];
-export const radiuses: Radius[] = ["0", "0.3", "0.5", "0.8", "1"];
+export const RADIUS_SIZES: Radius[] = [
+  { display: "Sharp", size: "0" },
+  { display: "Slight", size: "0.3" },
+  { display: "Medium", size: "0.5" },
+  { display: "Smooth", size: "0.75" },
+  { display: "Curved", size: "1" },
+];
 
 type providerType = "openai" | "google" | "anthropic" | "mistral";
 
 const providers: Record<
-	providerType,
-	{
-		models: model[];
-		active: boolean;
-		Icon: React.FC<SVGProps<SVGSVGElement>>;
-	}
+  providerType,
+  {
+    models: model[];
+    active: boolean;
+    Icon: React.FC<SVGProps<SVGSVGElement>>;
+  }
 > = {
-	openai: {
-		models: [
-			{ id: "gpt-5-mini", name: "GPT-5 mini" },
-			{ id: "gpt-5", name: "GPT-5" },
-			{ id: "gpt-5-nano", name: "GPT-5 nano" },
-		],
-		active: true,
-		Icon: openai,
-	},
-	google: {
-		models: [
-			{ id: "gemma-3-12b-it", name: "Gemma 3 12b " },
-			{ id: "gemma-3-27b-it", name: "Gemma 3 27b " },
-			{ id: "gemini-2.5-flash-lite", name: "Gemini 2.5 Flash Lite" },
-			{ id: "gemini-2.5-pro", name: "Gemini 2.5 Pro" },
-			{ id: "gemini-2.5-flash", name: "Gemini 2.5 Flash" },
-		],
-		active: true,
-		Icon: gemini,
-	},
-	anthropic: {
-		models: [
-			{ id: "claude-3-sonnet-20240229", name: "Claude Sonnet 3 " },
-			{ id: "claude-3-5-haiku-latest", name: "Claude Haiku 3.5 " },
-			{ id: "claude-3-7-sonnet-20250219", name: "Claude Sonnet 3.7" },
-			{ id: "claude-opus-4-20250514", name: "Claude Opus 4" },
-			{ id: "claude-sonnet-4-20250514", name: "Claude Sonnet 4" },
-		],
-		active: true,
-		Icon: claude,
-	},
-	mistral: {
-		models: [
-			{ id: "mistral-large-latest", name: "mistral large" },
-			{ id: "pixtral-large-latest", name: "pixtral large" },
-			{ id: "mistral-small-latest", name: "mistral small" },
-		],
-		active: true,
-		Icon: mistral,
-	},
+  openai: {
+    models: [
+      { id: "gpt-5-mini", name: "GPT-5 mini" },
+      { id: "gpt-5", name: "GPT-5" },
+      { id: "gpt-5-nano", name: "GPT-5 nano" },
+    ],
+    active: true,
+    Icon: openai,
+  },
+  google: {
+    models: [
+      { id: "gemma-3-12b-it", name: "Gemma 3 12b " },
+      { id: "gemma-3-27b-it", name: "Gemma 3 27b " },
+      { id: "gemini-2.5-flash-lite", name: "Gemini 2.5 Flash Lite" },
+      { id: "gemini-2.5-pro", name: "Gemini 2.5 Pro" },
+      { id: "gemini-2.5-flash", name: "Gemini 2.5 Flash" },
+    ],
+    active: true,
+    Icon: gemini,
+  },
+  anthropic: {
+    models: [
+      { id: "claude-3-sonnet-20240229", name: "Claude Sonnet 3 " },
+      { id: "claude-3-5-haiku-latest", name: "Claude Haiku 3.5 " },
+      { id: "claude-3-7-sonnet-20250219", name: "Claude Sonnet 3.7" },
+      { id: "claude-opus-4-20250514", name: "Claude Opus 4" },
+      { id: "claude-sonnet-4-20250514", name: "Claude Sonnet 4" },
+    ],
+    active: true,
+    Icon: claude,
+  },
+  mistral: {
+    models: [
+      { id: "mistral-large-latest", name: "mistral large" },
+      { id: "pixtral-large-latest", name: "pixtral large" },
+      { id: "mistral-small-latest", name: "mistral small" },
+    ],
+    active: true,
+    Icon: mistral,
+  },
 };
 
 export const models = Object.entries(providers)
-	.flatMap(([provider, value]) =>
-		value.models.map((model) => ({
-			provider,
-			...model,
-			active: value.active,
-			Icon: value.Icon,
-		})),
-	)
-	.filter((models) => models.active);
+  .flatMap(([provider, value]) =>
+    value.models.map((model) => ({
+      provider,
+      ...model,
+      active: value.active,
+      Icon: value.Icon,
+    }))
+  )
+  .filter((models) => models.active);
 
 export interface EditorBehaviorOption {
-	label: string;
-	value?: boolean;
-	description: string;
+  label: string;
+  value?: boolean;
+  description: string;
 }
--- src/types/editor.ts
+++ src/types/editor.ts
@@ -3,33 +3,40 @@ import type { editor } from "monaco-editor";
 import type { ConsoleOutput } from "./worker";
 
 export interface Theme {
-	name: string;
-	value: string;
-	ui: {
-		background: string;
-		foreground: string;
-		border: string;
-		accent: string;
-		selection: string;
-		header: string;
-		hover: string;
-		muted: string;
-		success: string;
-		warning: string;
-		error: string;
-		info: string;
-	};
-	monaco: editor.IStandaloneThemeData;
+  name: string;
+  value: string;
+  ui: {
+    background: string;
+    foreground: string;
+    border: string;
+    accent: string;
+    selection: string;
+    header: string;
+    hover: string;
+    muted: string;
+    success: string;
+    warning: string;
+    error: string;
+    info: string;
+  };
+  monaco: editor.IStandaloneThemeData;
 }
 
 export interface Tab {
-	id: string;
-	name: string;
-	language: string;
-	code: string;
-	logs: ConsoleOutput[];
-	logsFormated: string;
-	editing?: boolean;
+  id: string;
+  name: string;
+  language: string;
+  code: string;
+  logs: ConsoleOutput[];
+  logsFormated: string;
+  editing?: boolean;
 }
 
 export type lineRendererEditor = "none" | "gutter" | "line" | "all";
+
+export type RadiusSize = "0" | "0.3" | "0.5" | "0.75" | "1";
+export type Radius = {
+  display: string;
+  size: RadiusSize;
+};
+export type TLayout = "vertical" | "horizontal";
