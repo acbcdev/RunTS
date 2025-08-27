829d0c0ead7e34fdf769aab61b3e92861d264874
acbc <acbc.dev@gmail.com>
2025-08-27 13:36:44 -0500

    feat: add Loader component for loading indication

---
+++ src/components/ui/loader.tsx
@@ -0,0 +1,26 @@
+
+type LoaderProps = {
+  className?: string;
+};
+
+export function Loader({ className }: LoaderProps) {
+  return (
+    <div className={className}>
+      <div className="spinner">
+      <div className="spinner-blade"></div>
+      <div className="spinner-blade"></div>
+      <div className="spinner-blade"></div>
+      <div className="spinner-blade"></div>
+      <div className="spinner-blade"></div>
+      <div className="spinner-blade"></div>
+      <div className="spinner-blade"></div>
+      <div className="spinner-blade"></div>
+      <div className="spinner-blade"></div>
+      <div className="spinner-blade"></div>
+      <div className="spinner-blade"></div>
+      <div className="spinner-blade"></div>
+    </div>
+    </div>
+  
+  );
+}
