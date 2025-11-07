995f1fcf137350a1060e9e95837d1e0a78bc87b0
acbc <acbc.dev@gmail.com>
2025-11-06 21:27:38 -0500

    build(app): remove test configuration from vite config

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -5,10 +5,6 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
-  test: {
-    environment: "happy-dom", // Provides browser APIs like crypto, localStorage
-    globals: true,
-  },
   plugins: [
     react(),
     VitePWA({
