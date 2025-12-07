a679fc49b9988787e2633ebb7a46dfaadc46d465
acbc <acbc.dev@gmail.com>
2025-12-07 00:16:49 -0500

    refactor(prompt): improve code formatting and consistency

--- packages/app/src/features/ai/prompt/PrompInput.tsx
+++ packages/app/src/features/ai/prompt/PrompInput.tsx
@@ -6,92 +6,92 @@ import { useShallow } from "zustand/react/shallow";
 import { useTabsStore } from "@/features/tabs/tabs-store/tabs";
 import { Button } from "@/features/ui/button";
 import {
-	PromptInput,
-	PromptInputAction,
-	PromptInputActions,
-	PromptInputTextarea,
+  PromptInput,
+  PromptInputAction,
+  PromptInputActions,
+  PromptInputTextarea,
 } from "@/features/ui/prompt-input";
 import { useAIConfigStore } from "../store/aiConfig";
 import { ComboboxSelect } from "./ComboxSelector";
 import type { PromptInputProps } from "./types";
 
 export function Prompt({
-	value,
-	onValueChange,
-	isLoading,
-	onSubmit,
-	stop,
+  value,
+  onValueChange,
+  isLoading,
+  onSubmit,
+  stop,
 }: PromptInputProps) {
-	const contextFile = useAIConfigStore(
-		useShallow((state) => state.contextFile),
-	);
-	const setContextFile = useAIConfigStore(
-		useShallow((state) => state.setContextFile),
-	);
-	const tab = useTabsStore((state) => state.getCurrentTab());
-	const handleSubmit = () => {
-		if (value.trim()) {
-			onSubmit();
-		}
-	};
-	const currentFile = useMemo(() => {
-		const MAX_LENGTH = 12;
-		if (!tab?.name) return "Current Tab";
-		return tab?.name.length > MAX_LENGTH
-			? `${tab?.name.slice(0, MAX_LENGTH)}...`
-			: tab?.name;
-	}, [tab]);
+  const contextFile = useAIConfigStore(
+    useShallow((state) => state.contextFile)
+  );
+  const setContextFile = useAIConfigStore(
+    useShallow((state) => state.setContextFile)
+  );
+  const tab = useTabsStore((state) => state.getCurrentTab());
+  const handleSubmit = () => {
+    if (value.trim()) {
+      onSubmit();
+    }
+  };
+  const currentFile = useMemo(() => {
+    const MAX_LENGTH = 12;
+    if (!tab?.name) return "Current Tab";
+    return tab?.name.length > MAX_LENGTH
+      ? `${tab?.name.slice(0, MAX_LENGTH)}...`
+      : tab?.name;
+  }, [tab]);
 
-	return (
-		<PromptInput
-			value={value}
-			onValueChange={onValueChange}
-			isLoading={isLoading}
-			onSubmit={handleSubmit}
-			className="w-full max-w-(--breakpoint-md)  "
-		>
-			<PromptInputTextarea
-				className="max-h-32 dark:bg-transparent"
-				placeholder="Ask me anything..."
-			/>
+  return (
+    <PromptInput
+      value={value}
+      onValueChange={onValueChange}
+      isLoading={isLoading}
+      onSubmit={handleSubmit}
+      className="w-full max-w-(--breakpoint-md)  "
+    >
+      <PromptInputTextarea
+        className="max-h-32 dark:bg-transparent"
+        placeholder="Ask me anything..."
+      />
 
-			<PromptInputActions className="flex items-center justify-between gap-2 pt-2">
-				<div className="flex items-center gap-2">
-					<ComboboxSelect />
-					<Button
-						variant={"currentFile"}
-						type="button"
-						// size={"sm"}
-						className={`${!contextFile && "border-dashed"} `}
-						onClick={() => setContextFile(!contextFile)}
-					>
-						<span
-							className={`${!contextFile && "line-through"} hidden md:inline-block`}
-						>
-							{currentFile}
-						</span>
-						{/* <span className="text-muted ">Current Tab</span>{" "} */}
-						{contextFile ? <Eye /> : <EyeOff />}
-					</Button>
-				</div>
+      <PromptInputActions className="flex items-center justify-between gap-2 pt-2">
+        <div className="flex items-center gap-2">
+          <ComboboxSelect />
+          <Button
+            variant={"currentFile"}
+            type="button"
+            // size={"sm"}
+            className={`${!contextFile && "border-dashed"} `}
+            onClick={() => setContextFile(!contextFile)}
+          >
+            <span
+              className={`${!contextFile && "line-through"} hidden md:inline-block`}
+            >
+              {currentFile}
+            </span>
+            {/* <span className="text-muted ">Current Tab</span>{" "} */}
+            {contextFile ? <Eye /> : <EyeOff />}
+          </Button>
+        </div>
 
-				<PromptInputAction
-					tooltip={isLoading ? "Stop generation" : "Send message"}
-				>
-					<Button
-						variant="default"
-						size="icon"
-						className="h-8 w-8 rounded-full"
-						onClick={isLoading ? handleSubmit : stop}
-					>
-						{isLoading ? (
-							<Square className="size-5 fill-current" />
-						) : (
-							<ArrowUp className="size-5" />
-						)}
-					</Button>
-				</PromptInputAction>
-			</PromptInputActions>
-		</PromptInput>
-	);
+        <PromptInputAction
+          tooltip={isLoading ? "Stop generation" : "Send message"}
+        >
+          <Button
+            variant="default"
+            size="icon"
+            className="h-8 w-8 rounded-full"
+            onClick={isLoading ? stop : handleSubmit}
+          >
+            {isLoading ? (
+              <Square className="size-5 fill-current" />
+            ) : (
+              <ArrowUp className="size-5" />
+            )}
+          </Button>
+        </PromptInputAction>
+      </PromptInputActions>
+    </PromptInput>
+  );
 }
