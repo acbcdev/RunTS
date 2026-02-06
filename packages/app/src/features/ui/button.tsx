353ae8febcf8eec2ccb95f93634f80f90c63eecf
acbc <acbc.dev@gmail.com>
2026-02-05 23:30:06 -0500

    refactor(button): enhance buttonVariants structure and improve readability

--- packages/app/src/features/ui/button.tsx
+++ packages/app/src/features/ui/button.tsx
@@ -5,56 +5,58 @@ import type * as React from "react";
 import { cn } from "../lib/cn";
 
 const buttonVariants = cva(
-	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-hidden focus-visible:ring-1  focus-visible:ring-ring disabled:pointer-events-none  disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
-	{
-		variants: {
-			variant: {
-				default: "bg-muted text-foreground shadow-sm hover:bg-muted/90",
-				destructive:
-					"bg-destructive text-destructive-foreground shadow-xs hover:bg-destructive/90",
-				outline:
-					"border border-input bg-background shadow-xs hover:bg-muted hover:text-accent-foreground",
-				secondary:
-					"bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
-				ghost: "hover:bg-muted hover:text-accent",
-				link: "text-primary underline-offset-4 hover:underline",
-				border:
-					"border border-input bg-background shadow-xs border-accent hover:bg-input hover:border-background hover:text-accent",
-				currentFile: "bg-trasparent text-accent-foreground shadow-xs border ",
-			},
-			size: {
-				default: "h-9 px-4 py-2",
-				sm: "h-8 rounded-md px-3 text-xs",
-				lg: "h-10 rounded-md px-8",
-				icon: "h-9 w-9",
-			},
-		},
-		defaultVariants: {
-			variant: "default",
-			size: "default",
-		},
-	},
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-hidden focus-visible:ring-1  focus-visible:ring-ring disabled:pointer-events-none  disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  {
+    variants: {
+      variant: {
+        default: "bg-muted text-foreground shadow-sm hover:bg-muted/90",
+        destructive:
+          "bg-destructive text-destructive-foreground shadow-xs hover:bg-destructive/90",
+        outline:
+          "border border-input bg-background shadow-xs hover:bg-muted hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+        ghost: "hover:bg-muted hover:text-accent",
+        link: "text-primary underline-offset-4 hover:underline",
+        border:
+          "border border-input bg-background shadow-xs border-accent hover:bg-input hover:border-background hover:text-accent",
+        borderSettings:
+          "border border-input bg-background shadow-xs border-accent hover:bg-input hover:border-background hover:text-accent hover:border-accent hover:border",
+        currentFile: "bg-trasparent text-accent-foreground shadow-xs border ",
+      },
+      size: {
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rounded-md px-3 text-xs",
+        lg: "h-10 rounded-md px-8",
+        icon: "h-9 w-9",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
 );
 
 function Button({
-	className,
-	variant,
-	size,
-	asChild = false,
-	...props
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
 }: React.ComponentProps<"button"> &
-	VariantProps<typeof buttonVariants> & {
-		asChild?: boolean;
-	}) {
-	const Comp = asChild ? Slot : "button";
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot : "button";
 
-	return (
-		<Comp
-			data-slot="button"
-			className={cn(buttonVariants({ variant, size, className }))}
-			{...props}
-		/>
-	);
+  return (
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
 }
 
 export { Button, buttonVariants };
