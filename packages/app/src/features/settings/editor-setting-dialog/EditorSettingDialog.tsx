3dc68a94d7b233a074548ae25d6c0c5cdd0ba4af
acbc <acbc.dev@gmail.com>
2026-05-28 16:56:58 -0500

    refactor(EditorSettingsDialog): format code for improved readability and maintainability

--- packages/app/src/features/settings/editor-setting-dialog/EditorSettingDialog.tsx
+++ packages/app/src/features/settings/editor-setting-dialog/EditorSettingDialog.tsx
@@ -2,11 +2,11 @@ import { BrainCog, Brush, FileCode2, MonitorCog } from "lucide-react";
 import { useModalStore } from "@/features/common/modal/modal";
 import { versionApp } from "@/features/common/utils/const";
 import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
 } from "@/features/ui/dialog";
 import { ScrollArea } from "@/features/ui/scroll-area";
 import { Tabs, TabsList, TabsTrigger } from "@/features/ui/tabs";
@@ -14,86 +14,86 @@ import { GithubStars } from "../github-stars/GithubStars";
 import { AI, Appearance, Editor, Formatting } from "../sections";
 
 const Trigers = [
-	{ name: "Appearance", Icon: Brush },
-	{ name: "Editor", Icon: MonitorCog },
-	{ name: "Formatting", Icon: FileCode2 },
-	{ name: "AI", Icon: BrainCog },
+  { name: "Appearance", Icon: Brush },
+  { name: "Editor", Icon: MonitorCog },
+  { name: "Formatting", Icon: FileCode2 },
+  { name: "AI", Icon: BrainCog },
 ];
 export function EditorSettingsDialog() {
-	const open = useModalStore((state) => state.settings);
-	const toggle = useModalStore((state) => state.toggleModal);
-	return (
-		<Dialog
-			open={open}
-			onOpenChange={() => toggle("settings")}
-			aria-description="Editor settings"
-		>
-			<DialogContent
-				aria-describedby="editor-settings-dialog-description"
-				aria-description="Editor settings"
-				className="p-0 overflow-hidden xl:max-w-7xl lg:max-w-3xl"
-			>
-				<DialogHeader className="p-6 pb-0">
-					<DialogTitle>
-						Editor Settings <span className="opacity-85">{versionApp}</span>
-					</DialogTitle>
-					<DialogDescription>Configure the editor settings</DialogDescription>
-					<div className="flex items-center flex-wrap gap-2 mt-2 ">
-						<a
-							href="https://www.buymeacoffee.com/runts"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<img
-								src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png"
-								alt="Buy Me A Coffee"
-								className="h-10 my-2 aspect-[3.56]"
-							/>
-						</a>
-						<GithubStars />
-						<a
-							href="https://github.com/acbcdev/runts/issues/new"
-							target="_blank"
-							rel="noreferrer"
-							className="bg-input text-lg border  rounded-lg px-3 py-2  font-semibold"
-						>
-							Feedback
-						</a>
-						<a
-							href="https://github.com/acbcdev/runts/issues/new"
-							target="_blank"
-							rel="noreferrer"
-							className="bg-input border rounded-lg px-3 py-2 text-lg font-semibold"
-						>
-							Report a bug
-						</a>
-						<a
-							href="https://github.com/acbcdev/RunTS/blob/main/CHANGELOG.md"
-							target="_blank"
-							rel="noreferrer"
-							className="bg-input text-lg border  rounded-lg px-3 py-2  font-semibold"
-						>
-							Changelog
-						</a>
-					</div>
-				</DialogHeader>
+  const open = useModalStore((state) => state.settings);
+  const toggle = useModalStore((state) => state.toggleModal);
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={() => toggle("settings")}
+      aria-description="Editor settings"
+    >
+      <DialogContent
+        aria-describedby="editor-settings-dialog-description"
+        aria-description="Editor settings"
+        className="p-0 overflow-hidden xl:max-w-7xl lg:max-w-3xl"
+      >
+        <DialogHeader className="p-6 pb-0">
+          <DialogTitle>
+            Editor Settings <span className="opacity-85">{versionApp}</span>
+          </DialogTitle>
+          <DialogDescription>Configure the editor settings</DialogDescription>
+          <div className="flex items-center flex-wrap gap-2 mt-2 ">
+            <a
+              href="https://www.buymeacoffee.com/runts"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png"
+                alt="Buy Me A Coffee"
+                className="h-10 my-2 aspect-[3.56]"
+              />
+            </a>
+            <GithubStars />
+            <a
+              href="https://github.com/acbcdev/runts/issues/new"
+              target="_blank"
+              rel="noreferrer"
+              className="bg-input text-lg border  rounded-lg px-3 py-2  font-semibold"
+            >
+              Feedback
+            </a>
+            <a
+              href="https://github.com/acbcdev/runts/issues/new"
+              target="_blank"
+              rel="noreferrer"
+              className="bg-input border rounded-lg px-3 py-2 text-lg font-semibold"
+            >
+              Report a bug
+            </a>
+            <a
+              href="https://github.com/acbcdev/RunTS/blob/main/packages/app/CHANGELOG.md"
+              target="_blank"
+              rel="noreferrer"
+              className="bg-input text-lg border  rounded-lg px-3 py-2  font-semibold"
+            >
+              Changelog
+            </a>
+          </div>
+        </DialogHeader>
 
-				<Tabs defaultValue="appearance" className="flex-1">
-					<TabsList className="h-12 px-6 justify-start">
-						{Trigers.map(({ name, Icon }) => (
-							<TabsTrigger key={name} value={name.toLowerCase()}>
-								<Icon className="size-4 mr-2" /> {name}
-							</TabsTrigger>
-						))}
-					</TabsList>
-					<ScrollArea className="max-h-[85dvh] h-[60dvh] ">
-						<Appearance />
-						<Editor />
-						<Formatting />
-						<AI />
-					</ScrollArea>
-				</Tabs>
-			</DialogContent>
-		</Dialog>
-	);
+        <Tabs defaultValue="appearance" className="flex-1">
+          <TabsList className="h-12 px-6 justify-start">
+            {Trigers.map(({ name, Icon }) => (
+              <TabsTrigger key={name} value={name.toLowerCase()}>
+                <Icon className="size-4 mr-2" /> {name}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          <ScrollArea className="max-h-[85dvh] h-[60dvh] ">
+            <Appearance />
+            <Editor />
+            <Formatting />
+            <AI />
+          </ScrollArea>
+        </Tabs>
+      </DialogContent>
+    </Dialog>
+  );
 }
